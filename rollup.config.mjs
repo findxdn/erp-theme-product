f89f984f0f28358b82b761957287f1feddb3bc31
chienhv <hoangvanchien2206@gmail.com>
2023-09-24 18:46:16 +0700

    init

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,8 +2,10 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
+import postcss from 'rollup-plugin-postcss'
 
 import packageJson from "./package.json" assert { type: "json" };
+
 
 export default [
   {
@@ -24,11 +26,13 @@ export default [
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
+      postcss()
     ],
   },
   {
     input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
+    external: [/\.(css|less|scss)$/],
   },
 ];
